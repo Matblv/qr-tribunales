8f81becbb37ab2fbe9e7d09a855c52e86ad411cd
Matias Belvedere <matiasbelvedere@hotmail.com>
2023-10-21 17:03:01 -0300

    correcion button

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const body = document.getElementById("body");
 const alerta = document.getElementById("alerta");
 const background = document.getElementById("background");
+const button = document.getElementById("button");
 background.style.height = `${screen.height}px`;
 
 
-alerta.addEventListener("click", () => {
+button.addEventListener("click", () => {
     alerta.classList = "display";
     background.classList = "display";
     body.style.overflow = "auto"

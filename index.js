0fc9e96cb1da9813c91fe49b29d8818848601db2
Matias Belvedere <matiasbelvedere@hotmail.com>
2023-10-21 16:35:56 -0300

    primer final

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const body = document.getElementById("body");
 const alerta = document.getElementById("alerta");
 const background = document.getElementById("background");
+background.style.height = `${screen.height}px`;
+
 
 alerta.addEventListener("click", () => {
     alerta.classList = "display";
     background.classList = "display";
+    body.style.overflow = "auto"
 })

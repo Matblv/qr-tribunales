d5046c84ff89387f5cafd514bcba6e03a99b3b4c
Matias Belvedere <matiasbelvedere@hotmail.com>
2023-10-21 16:22:48 -0300

    2do test

---
+++ index.js
@@ -0,0 +1,7 @@
+const alerta = document.getElementById("alerta");
+const background = document.getElementById("background");
+
+alerta.addEventListener("click", () => {
+    alerta.classList = "display";
+    background.classList = "display";
+})
